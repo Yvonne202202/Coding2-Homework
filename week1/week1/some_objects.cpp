caefac2ef8aac379a74f2ad53ebdcff2b8517963
Yvonne <115041069+Yvonne202202@users.noreply.github.com>
2023-01-24 20:42:29 +0000

    Add files via upload

---
+++ week1/week1/some_objects.cpp
@@ -0,0 +1,12 @@
+//
+//  some_objects.cpp
+//  week1
+//
+//  Created by 王钰 on 17/01/2023.
+//
+
+#include "some_objects.hpp"
+
+float myClass::myFounc(int x, float y){
+    return x * y;
+}
---
+++ week1/week1/some_objects.hpp
@@ -0,0 +1,19 @@
+//
+//  some_objects.hpp
+//  week1
+//
+//  Created by 王钰 on 17/01/2023.
+//
+
+#ifndef some_objects_hpp
+#define some_objects_hpp
+
+#include <stdio.h>
+class myClass{
+public:
+    int x = 10;
+    float y = 3.1415;
+    float myFounc(int x, float y);
+};
+
+#endif /* some_objects_hpp */
